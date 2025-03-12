6bc35370743a807bc319ffc71d7acfb86020cb31
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-03-12 19:11:50 +0100

    feat: add snack 8

--- ex-js-array-methods-main/snack-8/script.js
+++ ex-js-array-methods-main/snack-8/script.js
@@ -21,3 +21,7 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const student = students.find((person) => person.name === 'Marco Lanci');
+
+console.log(student.class);
